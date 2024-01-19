68681a212733760ba4c76d8d17d8f212ae0dd0c3
ArttuTaht <arttu.tahtinen@protonmail.com>
2024-01-19 13:41:51 +0200

    Adding functional code to the git-repository

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  const toggle = () => {
+    setModalVisible(prevVisible => !prevVisible)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={toggle}>
+        <Text>
+          Show modal message
+        </Text>
+      </Pressable>
+      {modalVisible && (
+        <Modal
+          visible={modalVisible}
+          onRequestClose={() => {setModalVisible(!modalVisible);
+        }}>
+          <View style={styles.container}>
+            <View style={styles.modalView}>
+              <Text>This is modal...</Text>
+              <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                <Text>Close</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      )}
     </View>
   );
 }
@@ -17,4 +41,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  }
 });
